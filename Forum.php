e51548491971b2858065b17ce7654875bfef102e
Rafael Gabardo <rggabardo@gmail.com>
2023-05-04 19:43:43 -0300

    Adicionando os arquivos principais

---
+++ Forum.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Forum.css">
+    <title>Fórum</title>
+</head>
+<body>
+    <nav class="nav">
+        <div class="block">
+
+        </div>
+        <ul class="nav-list">
+            <div class="div-nav">
+                <li class="cadastro"><a class="login" href="index.php">Log in</a></li>
+            </div>
+            <div class="div-nav">
+                <li class="cadastro"><a class="signin" href="SignIn.php">Sign up</a></li>
+            </div>
+        </ul>
+    </nav>
+    <main class="main">
+        <section class="section-menu">
+            <div class="div-menu">
+                <ul class="menu-list">
+                    <li class="menu"><h1>Páginas</h1></li>
+                    <li class="menu"><a class="pages" href="">Fórum Geral</a></li>
+                    <li class="menu"><a class="pages" href="">Amigos</a></li>
+                </ul>
+            </div>
+        </section>
+        <section class="section-posts">
+            <div class="div-posts">
+                <h1 class="h1-posts">Postagens</h1>
+                <p class="paragraph-posts">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br>
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br>
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br>
+                    It has survived not only five centuries, but also the leap into electronic typesetting, <br>
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, <br>
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
